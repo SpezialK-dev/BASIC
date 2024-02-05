817e21e718af6d862ec9f05e2f432e1a6135d664
SpezialK-dev <spezcialK@protonmail.com>
2024-02-05 19:05:02 +0100

    variabel name writing works

--- src/variable_handling.cpp
+++ src/variable_handling.cpp
@@ -1,36 +1,29 @@
-
-
-
-
 class variable_handling
 {
 private:
     //128 Variable names
     //20 char max lenght rest ist discarded
-    char variable_names[128][20]={""};
-    void* value_array[128];
+    char variable_names[128][21]={""};
+    void* value_array[128]; 
+    char find_space_in_variable(char *name);
 public:
-    //variable_handling(/* args */);
-    //~variable_handling();
     void create_new_variable(char  name[], char value[]);
     char& get_variable_name(char token);
     void* get_value(char token);
     void delete_variabel(char token);
+
 };
-
-//variable_handling::variable_handling(/* args */)
-//{
-//}
-//
-//variable_handling::~variable_handling()
-//{
-//}
-
 
 //creation of new variable
 // does not return anything might return a statuscode in the future as a char.
 void variable_handling::create_new_variable(char  name[], char value[]){
     
+    // copying name
+    char working_index = find_space_in_variable(name);
+    for(int i = 0; i< 19; ++i){
+        variable_names[working_index][i] = name[i];
+    };
+    variable_names[working_index][20] = '\0';
 }
 // returns pointer to char array of that type,
 // token is the token give by the tokenzier, this is needed for things like scrolleback
@@ -47,4 +40,10 @@ void* variable_handling::get_value(char token){
 // cleans up after variable
 void variable_handling::delete_variabel(char token){
     
+}
+
+//supposed to look through array and find available spot where to put 
+//the variable based on name 
+char variable_handling::find_space_in_variable(char *name){
+    return 1;
 }
--- src/varianle_handling.h
+++ src/varianle_handling.h
@@ -1,14 +1,10 @@
-
 class variable_handling
 {
 private:
-    //128 Variable names
-    //20 char max lenght rest ist discarded
     char variable_names[128][20]={""};
     void* value_array[128];
+    char find_space_in_variable(char *name);
 public:
-    //variable_handling(/* args */);
-    //~variable_handling();
     void create_new_variable(char  name[], char value[]);
     char& get_variable_name(char token);
     void* get_value(char token);
