9bd723fa6969e9ca491d33f4ff338b224a1a4187
SpezialK-dev <spezcialK@protonmail.com>
2024-02-05 18:31:28 +0100

    converted to class

--- src/variable_handling.cpp
+++ src/variable_handling.cpp
@@ -1,20 +1,50 @@
 
-//128 Variable names
-//20 char max lenght rest ist discarded
-char variable_names[128][20]={""};
-void* value_array[128];
+
+
+
+class variable_handling
+{
+private:
+    //128 Variable names
+    //20 char max lenght rest ist discarded
+    char variable_names[128][20]={""};
+    void* value_array[128];
+public:
+    //variable_handling(/* args */);
+    //~variable_handling();
+    void create_new_variable(char  name[], char value[]);
+    char& get_variable_name(char token);
+    void* get_value(char token);
+    void delete_variabel(char token);
+};
+
+//variable_handling::variable_handling(/* args */)
+//{
+//}
+//
+//variable_handling::~variable_handling()
+//{
+//}
+
 
 //creation of new variable
 // does not return anything might return a statuscode in the future as a char.
-void create_new_variable(char  name[], char& type, char value[]){
-
+void variable_handling::create_new_variable(char  name[], char value[]){
+    
 }
 // returns pointer to char array of that type,
 // token is the token give by the tokenzier, this is needed for things like scrolleback
-char& get_variable_name(char token){
-    
+char& variable_handling::get_variable_name(char token){
+     return *variable_names[token];
 }
 
-void delete_variabel(int position){
-
+//should just return the value of the array at the point of the token
+void* variable_handling::get_value(char token){
+    return value_array[token];
+}
+ 
+// returns nothing 
+// cleans up after variable
+void variable_handling::delete_variabel(char token){
+    
 }
--- src/varianle_handling.h
+++ src/varianle_handling.h
@@ -1,5 +1,16 @@
 
-
-char* get_variable_name(char token);
-
-void create_new_variable(char name[], char type);
+class variable_handling
+{
+private:
+    //128 Variable names
+    //20 char max lenght rest ist discarded
+    char variable_names[128][20]={""};
+    void* value_array[128];
+public:
+    //variable_handling(/* args */);
+    //~variable_handling();
+    void create_new_variable(char  name[], char value[]);
+    char& get_variable_name(char token);
+    void* get_value(char token);
+    void delete_variabel(char token);
+};
