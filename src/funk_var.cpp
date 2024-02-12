cffb6aecffae02eb8e054b41a67c72d7b64df253
SpezialK-dev <spezcialK@protonmail.com>
2024-02-12 20:01:18 +0100

    fixed type error so that it actually accepts pointers

--- src/funk_var.cpp
+++ src/funk_var.cpp
@@ -1,1 +1,16 @@
 #include "funk_var.h"
+
+
+void* funk_var::get_func(){
+    return funtion_pointer;
+}
+unsigned char funk_var::get_return_type(){
+    return return_type;
+}
+
+char* funk_var::get_type_signature(){
+    return type_signature;
+}
+char* funk_var::get_name(){
+    return name;
+}
--- src/funk_var.h
+++ src/funk_var.h
@@ -1,12 +1,12 @@
 class funk_var
 {
 private:
-    void* funtion_pointer;
-    unsigned char return_type;
+    void (*funtion_pointer)();
+    char return_type;
     char type_signature[10];
     char name[25];
 public:
-    funk_var(void* func_pointer, unsigned char return_type,
+    funk_var(void (*funtion_pointer)(), unsigned char return_type,
             char type_signature[], char name[]
             );
     void* get_func();
@@ -15,3 +15,17 @@ public:
     char* get_name();
        
 };
+//this requires the arrays to be the same size!!
+//but it should not matter in the context of this class!
+void copy_array(int lenght, char* to_copy_to, char* copy_from){
+    for(int i = 0; i < lenght; ++i){
+        *(to_copy_to+i) = *(to_copy_to+i);
+    }
+}
+
+funk_var::funk_var(void (*inp_funtion_pointer)(), unsigned char inp_return_type, char inp_type_signature[], char inp_name[]){
+    funtion_pointer = *inp_funtion_pointer;
+    return_type = inp_return_type;
+    copy_array(10, type_signature, inp_type_signature);
+    copy_array(25, name, inp_name);
+}
