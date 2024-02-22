6277a51980330d13eb9fa11023deaa8cbfb0b012
SpezialK-dev <spezcialK@protonmail.com>
2024-02-22 20:22:19 +0100

    made stringlib static so it can be used without creating a new class of it and finished search through functions

--- src/editor/tokenizer.cpp
+++ src/editor/tokenizer.cpp
@@ -1,11 +1,15 @@
 #include "tokenizer.h"
+#include "../lib/string_lib.h"
 
 
 int tokenizer::search_through_funktions(funktionstable funk_table, char* input_arr){
     for(int i = 0; i< 40; ++i){
         funk_var funk = funk_table.getfunk_var(i);
-        
-    } 
+        if(string_lib::isequal(funk.get_name(), input_arr)){
+            return i;
+        }
+    }
+    return 999;
 }
 
 int tokenizer::search_through_variables(funktionstable funk_table, char* input_arr){
--- src/lib/string_lib.h
+++ src/lib/string_lib.h
@@ -3,6 +3,6 @@ class string_lib
 private:
     
 public:
-   int search_for_substring(char* search_in, char* what_to_search_for);
-   bool isequal(char* a, char* b);
+   static int search_for_substring(char* search_in, char* what_to_search_for);
+   static bool isequal(char* a, char* b);
 };
