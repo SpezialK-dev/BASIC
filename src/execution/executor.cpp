3ca77dd55599fb285764ffb3b29387c06ead7031
SpezialK-dev <spezcialK@protonmail.com>
2024-03-11 20:57:40 +0100

    added initals for execution

--- src/execution/executor.cpp
+++ src/execution/executor.cpp
@@ -1,2 +1,23 @@
 #include "executor.h"
 
+void executor::execute(editing_window *window){
+    bool running =true;
+    while(running){
+        for(int i = 0; i< 120; ++i){
+            //code to loading the current line into the buffers
+            if(*((*window).get_linebuffer_line(current_line)+i) == '\0'){
+                break;
+            }
+            //funktion check
+            if(*((*window).get_linebuffer_line(current_line)+i) >= 16&& *((*window).get_linebuffer_line(current_line)+i) <= 127 ){
+
+            }
+        }
+        //code for getting through the funktion array
+        for(int i = 0; i< 120; ++i){ //TODO check if 120 is enought not sure yet
+            
+        }
+
+        ++current_line;
+    }
+}
--- src/execution/executor.h
+++ src/execution/executor.h
@@ -2,14 +2,19 @@
 #define executor_h
 
 class b_var;
+#include "../editor/editing_window.h";
+
+
 class executor
 {
 private:
     int current_line;
+    b_var* (* func [40])(int number_param,b_var args[]);
     b_var* *current_variables = new b_var*[120];// we will never execde 120 variables in a single line if yes we have a major problem but allocating more space is wastefull
     
 public:
     inline executor();
+    void execute(editing_window *window);
 };
 
 executor::executor()
