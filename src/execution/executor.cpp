379893638d495624fdaf5285aae10dcf06b64f37
spezialk-dev <spezcialK@protonmail.com>
2024-03-12 22:15:01 +0100

    some small changes

--- src/execution/executor.cpp
+++ src/execution/executor.cpp
@@ -1,6 +1,6 @@
 #include "executor.h"
 
-void executor::execute(editing_window *window){
+void executor::execute(editing_window *window, funktionstable* funktable,variable_handling* variables_table,){
     bool running =true;
     while(running){
         for(int i = 0; i< 120; ++i){
@@ -9,7 +9,13 @@ void executor::execute(editing_window *window){
                 break;
             }
             //funktion check
-            if(*((*window).get_linebuffer_line(current_line)+i) >= 16&& *((*window).get_linebuffer_line(current_line)+i) <= 127 ){
+            //empty space from 56 till 127 since that is the space not in use rn.
+            if(*((*window).get_linebuffer_line(current_line)+i) >= 16 && *((*window).get_linebuffer_line(current_line)+i) < 56 ){
+                (*((*window).get_linebuffer_line(current_line)+i) -16);
+            }
+            //variable code 
+            //255 should be the max number if I remeber correctly, will have to check again through
+            if(*((*window).get_linebuffer_line(current_line)+i) >= 128 && *((*window).get_linebuffer_line(current_line)+i) < 255 ){
 
             }
         }
--- src/execution/executor.h
+++ src/execution/executor.h
@@ -2,24 +2,27 @@
 #define executor_h
 
 class b_var;
-#include "../editor/editing_window.h";
-
+#include "../editor/editing_window.h"
 
 class executor
 {
 private:
+    int func_index;
+    int var_index;
     int current_line;
     b_var* (* func [40])(int number_param,b_var args[]);
     b_var* *current_variables = new b_var*[120];// we will never execde 120 variables in a single line if yes we have a major problem but allocating more space is wastefull
     
 public:
     inline executor();
-    void execute(editing_window *window);
+    void execute(editing_window *window, funktionstable* funktable,variable_handling* variables_table,);
 };
 
 executor::executor()
 {
     current_line = 0;
+    func_index = 0;
+    var_index = 0;
 }
 
 #endif 
