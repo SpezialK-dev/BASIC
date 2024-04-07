7f81b89b873ba8636f8756c14c44eb202155c151
SpezialK-dev <spezcialK@protonmail.com>
2024-04-07 17:25:43 +0200

    adding a release skript to turn on optimizations

---
+++ release.sh
@@ -0,0 +1,1 @@
+g++ -o BASIC_realse -O3 ./src/main.cpp ./src/variables/variable_handling.cpp ./src/variables/b_var.cpp ./src/funktions/funk_var.cpp ./src/funktions/funktionstable.cpp ./src/funktions/funktions_dic.cpp ./src/editor/editing_window.cpp ./src/editor/tokenizer.cpp ./src/lib/string_lib.cpp ./src/execution/executor.cpp
