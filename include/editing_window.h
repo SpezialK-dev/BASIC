8d45c16822cfbfa83e053b4fdd0613f4be9b6378
spezialk-dev <spezcialK@protonmail.com>
2024-05-30 21:11:42 +0200

    refreactored code

--- include/editing_window.h
+++ include/editing_window.h
@@ -9,28 +9,28 @@ class variable_handling;
 class editing_window
 {
 private:
-    unsigned char **linebuffer;//[32767][120];//might have to optimize this cause this takes reserves a lot of memery should be around ~3-4 mbyte 
+  unsigned char **linebuffer;//[32767][120];//might have to optimize this cause this takes reserves a lot of memery should be around ~3-4 mbyte 
   bool line_set[MAX_LINE_NUMB];
     //values are abatrary 
-    const char error_msgs[20][250]{
-        "LINE TO LONG", 
-        "SYNTAX ERROR",
-        "TYPE MISSMATCH",
-        "READING/WRITING OUT OF LINE BUFFER SPACE"
-    };
-    int last_usedline;
-    bool currently_running;
-    bool add_line(funktionstable *funkt_table,variable_handling* variables_table);
-    int get_line_number(char* input_line);
-    void copy_to_line_buffer(unsigned char* input, int position);
+  const char error_msgs[20][250]{
+    "LINE TO LONG", 
+    "SYNTAX ERROR",
+    "TYPE MISSMATCH",
+    "READING/WRITING OUT OF LINE BUFFER SPACE"
+  };
+  int last_usedline;
+  bool currently_running;
+  bool add_line(funktionstable *funkt_table,variable_handling* variables_table);
+  int get_line_number(char* input_line);
+  void copy_to_line_buffer(unsigned char* input, int position);
     
 public:
-    void open_editor(funktionstable *funkt_table, variable_handling* variables_table);
-    unsigned char* get_linebuffer_line(int line_in_linebuffer);
-    void raise_error(int error_code);
-    editing_window();
+  void open_editor(funktionstable *funkt_table, variable_handling* variables_table);
+  unsigned char* get_linebuffer_line(int line_in_linebuffer);
+  void raise_error(int error_code);
+  editing_window();
   ~editing_window();
-    void end_editing_window();
+  void end_editing_window();
   bool get_linebuffer_bool(int line_in_linebuffer);
 };
 
